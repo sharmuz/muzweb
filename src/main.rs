0a630513b7131501bf7de8bb3eb97db79d276760
sharmuz <muzher@gmail.com>
2025-03-15 13:56:03 +0000

    Respond with 404 page if request not correct

--- src/main.rs
+++ src/main.rs
@@ -16,21 +16,27 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|res| res.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let (status_line, html_file) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 D'OH!", "404.html")
+    };
+
+    let response = create_response(&status_line, &html_file);
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
+fn create_response(status_line: &str, html_file: &str) -> String {
+    let contents = fs::read_to_string(html_file).unwrap();
     let length = contents.len();
-    
+
     let response = format!(
         "{status_line}\r\n\
         Content-Length: {length}\r\n\r\n\
         {contents}"
     );
 
-    stream.write_all(response.as_bytes()).unwrap();
+    return response
 }
